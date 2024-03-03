5bc2ad0ec29fbf5ceda87f54cf03fce47ded8637
sorraine <kalifasroberts@gmail.com>
2024-03-03 14:07:52 -0400

    Added client

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Client;
 
 class DashboardController extends Controller
 {
     public function index(): View
     {
-        return view('dashboard');
+        
+        $clients = Client::all();
+        return view('dashboard', ['clients' => $clients]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,4 +3,24 @@
 @section('content')
     <h1 class="display-5 fw-bold text-body-emphasis">Dashboard</h1>
     <p class="lead">Welcome to your dashboard. Here you can manage your account, your clients and much more.</p>
+
+    @if (session()->has('success'))
+        <div class="alert alert-success">
+            {{ session()->get('success') }}
+        </div>
+    @endif
+
+    @foreach ($clients as $client)
+    <div class="card text-center">
+        <img src="{{ url($client->company_logo) }}" alt="" class="card-img-top">
+        <div class="card-body">
+            <h4 class-title>{{ $client->name }}</h4>
+            <p class="card-text">
+                <strong>{{ $client->email }}</strong> <br>
+                <strong>{{ $client->telephone }}</strong> <br>
+                <strong>{{ $client->address }}</strong>
+            </p>
+        </div>
+    </div>
+    @endforeach
 @endsection
